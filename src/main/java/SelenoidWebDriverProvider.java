a1fd8d59b630893efec08aff9f05ac7e3dcf8ecf
321673 <>
2019-03-13 22:41:17 +0300

    Added SelenoidWebDriverProvider

---
+++ src/main/java/SelenoidWebDriverProvider.java
@@ -0,0 +1,34 @@
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class SelenoidWebDriverProvider {
+
+    public RemoteWebDriver createDriver() {
+
+       try{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("firefox");
+            capabilities.setVersion("latest");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", false);
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.setPreference("dom.webnotifications.enabled", false);
+            profile.setPreference("dom.push.enabled", false);
+            capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+            RemoteWebDriver driver = new RemoteWebDriver(
+                    URI.create("http://localhost:4444/wd/hub").toURL(),
+                    capabilities
+            );
+            driver.manage().window().setSize(new Dimension(1280, 1024));
+            return driver;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
